b9893a60724857d1fc931034ca002c44c3a0b230
Sowmitra <sowmitraguho@gmail.com>
2024-04-09 22:38:21 +0600

    some files added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-
+const cors = require('cors');
 app.use(express.json());
+
+const course = require('../CoursePlanetServer/Data/courses.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Course Portal Running...');
+});
 
 app.get("/", (req, res) => {
     res.status(200).send("Hello from the server!");
